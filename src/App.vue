e1270c2e17777658cd4fa95a1ce400ff18425abd
Duc Anh TA <83185055+taducanh1605@users.noreply.github.com>
2022-08-03 22:56:52 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -52,34 +52,6 @@ export default {
   }
 
 }
-
-/*----------------------------------------------------------------------
-Prevent Reload page when workout
-----------------------------------------------------------------------*/
-window.onbeforeunload = function() {
-  if (vm.time > 0){
-      return "Do you want to reload page?";
-  };
-};
-
-/*----------------------------------------------------------------------
-Prevent Lock Screen on mobile
-- first event: click
-- second event: hide tab
-----------------------------------------------------------------------*/
-var checkScreen = 0;
-document.addEventListener('click', async () => {
-  if (('wakeLock' in navigator) && (checkScreen == 0)) {
-      checkScreen = 1;
-      let screenLock = await navigator.wakeLock.request('screen');
-  };
-});
-
-document.addEventListener('visibilitychange', async () => {
-  if ('wakeLock' in navigator) {
-      let screenLock = await navigator.wakeLock.request('screen');
-  };
-});
 </script>
 
 <style>
